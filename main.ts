b0abc7867c7cfd2bef30283d5db0f2e2d6db194e
Jean-Pierre Bergamin <james.bergamin@local.ch>
2020-04-21 11:53:24 +0200

    WIP

--- main.ts
+++ main.ts
@@ -1,18 +1,49 @@
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     if (sprites.readDataNumber(otherSprite, "welk") == 1) {
+        sprite_list = sprites.allOfKind(SpriteKind.Food)
+        welkeBlumen = 0
+        for (let Wert of sprite_list) {
+            if (sprites.readDataNumber(Wert, "welk") == 1) {
+                welkeBlumen += 1
+            }
+        }
         mySprite.startEffect(effects.blizzard, 500)
         otherSprite.destroy()
         info.changeScoreBy(1)
+        if (welkeBlumen == 1) {
+            game.over(true)
+        }
     }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    game.over(false)
+    otherSprite.destroy(effects.fire, 100)
+    info.changeLifeBy(-1)
+    if (info.life() == 0) {
+        game.over(false)
+    }
 })
-let sprite_list: Sprite[] = []
-let seite = 0
+function plantFlower () {
+    blume = sprites.create(img`
+. . 5 5 5 . . . 
+. 5 5 5 5 5 . . 
+. 5 5 e 5 5 . . 
+. 5 5 5 5 5 . . 
+. . 5 5 5 . . . 
+. . . 7 . . . . 
+. 7 7 7 7 7 . . 
+. . . 7 . . . . 
+`, SpriteKind.Food)
+    mySprite.z = 5
+    blume.setPosition(Math.randomRange(5, scene.screenWidth()), Math.randomRange(5, scene.screenHeight()))
+    sprites.setDataNumber(blume, "ts", game.runtime())
+    sprites.setDataNumber(blume, "welk", 0)
+}
 let baum: Sprite = null
 let blume: Sprite = null
+let sprite_list: Sprite[] = []
 let mySprite: Sprite = null
+let welkeBlumen: number
+let seite = 1
 scene.setBackgroundColor(6)
 mySprite = sprites.create(img`
 . . . . . f 1 f 1 f . . . . . . 
@@ -35,63 +66,53 @@ mySprite = sprites.create(img`
 mySprite.setFlag(SpriteFlag.StayInScreen, true)
 mySprite.z = 10
 controller.moveSprite(mySprite)
-game.onUpdateInterval(1200, function () {
-    blume = sprites.create(img`
-. . 5 5 5 . . . 
-. 5 5 5 5 5 . . 
-. 5 5 e 5 5 . . 
-. 5 5 5 5 5 . . 
-. . 5 5 5 . . . 
-. . . 7 . . . . 
-. 7 7 7 7 7 . . 
-. . . 7 . . . . 
-`, SpriteKind.Food)
-    mySprite.z = 5
-    blume.setPosition(Math.randomRange(0, scene.screenWidth()), Math.randomRange(0, scene.screenHeight()))
-    sprites.setDataNumber(blume, "ts", game.runtime())
-    sprites.setDataNumber(blume, "welk", 0)
+info.setLife(5)
+plantFlower()
+plantFlower()
+game.onUpdateInterval(1500, function () {
+    plantFlower()
 })
 game.onUpdateInterval(500, function () {
-    if (Math.percentChance(50)) {
+    if (Math.percentChance(30)) {
         baum = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . 7 7 7 7 7 7 . . . . . 
-. . . . 7 7 . . . . . 7 . . . . 
-. . . . 7 . . . . . . 7 . . . . 
-. . . . . 7 . . . . . 7 . . . . 
-. . . . . . 7 . . . . 7 . . . . 
-. . . . . . . 7 7 7 7 . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
+. . . . . 7 7 7 7 7 . . . . . . 
+. . . 7 7 7 7 7 7 7 7 7 . . . . 
+. . . 7 7 7 7 7 7 7 7 7 7 . . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 7 . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 7 . 
+. . . . . . . 7 7 7 7 7 . . . . 
+. . . . . . . e e . . . . . . . 
+. . . . . . . e e . . . . . . . 
+. . . . . . . e e . . . . . . . 
+. . . . . . . e e . . . . . . . 
+. . . . . . . e e . . . . . . . 
+. . . . . . . e e . . . . . . . 
 `, SpriteKind.Enemy)
         seite = Math.randomRange(0, 3)
         if (seite == 0) {
-            baum.setVelocity(Math.randomRange(-30, 30), Math.randomRange(5, 30))
+            baum.setVelocity(Math.randomRange(-20, 20), Math.randomRange(5, 20))
             baum.setPosition(Math.randomRange(0, scene.screenWidth()), 0)
         } else if (seite == 1) {
-            baum.setVelocity(Math.randomRange(-30, -5), Math.randomRange(-30, 30))
+            baum.setVelocity(Math.randomRange(-20, -5), Math.randomRange(-20, 20))
             baum.setPosition(scene.screenWidth(), Math.randomRange(0, scene.screenHeight()))
         } else if (seite == 2) {
-            baum.setVelocity(Math.randomRange(-30, 30), Math.randomRange(-30, -5))
+            baum.setVelocity(Math.randomRange(-20, 20), Math.randomRange(-20, -5))
             baum.setPosition(Math.randomRange(0, scene.screenWidth()), scene.screenHeight())
         } else {
-            baum.setVelocity(Math.randomRange(5, 30), Math.randomRange(-30, 30))
+            baum.setVelocity(Math.randomRange(5, 20), Math.randomRange(-20, 20))
             baum.setPosition(0, Math.randomRange(0, scene.screenHeight()))
         }
     }
 })
 game.onUpdateInterval(700, function () {
     sprite_list = sprites.allOfKind(SpriteKind.Food)
-    for (let Wert of sprite_list) {
-        if (game.runtime() - sprites.readDataNumber(Wert, "ts") > 5000) {
-            Wert.setImage(img`
+    for (let Wert2 of sprite_list) {
+        if (game.runtime() - sprites.readDataNumber(Wert2, "ts") > 5000) {
+            Wert2.setImage(img`
 . . . . . . . . 
 . 1 . 1 . 1 . . 
 . . 1 1 1 . . . 
@@ -101,7 +122,7 @@ game.onUpdateInterval(700, function () {
 . . . 7 . . . . 
 . . . 7 . . . . 
 `)
-            sprites.setDataNumber(Wert, "welk", 1)
+            sprites.setDataNumber(Wert2, "welk", 1)
         }
     }
 })
